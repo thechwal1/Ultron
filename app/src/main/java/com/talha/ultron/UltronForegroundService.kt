4b17ee2b06836d96669f9dadf24b28d9ef9bb822
Talha Mahmood <talha@ultron.local>
2026-09-01 21:20:22 +0000

    Fix missing kotlinx.coroutines.cancel import in UltronForegroundService

--- app/src/main/java/com/talha/ultron/UltronForegroundService.kt
+++ app/src/main/java/com/talha/ultron/UltronForegroundService.kt
@@ -40,6 +40,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlin.math.abs
 
